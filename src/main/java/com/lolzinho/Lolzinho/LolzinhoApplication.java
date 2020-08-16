39675aab03be621f9fa9971897156fc723ca0c72
Rafael <macedo.rafanazareth@gmail.com>
2020-08-16 17:28:54 -0300

    Initial Commit

---
+++ src/main/java/com/lolzinho/Lolzinho/LolzinhoApplication.java
@@ -0,0 +1,13 @@
+package com.lolzinho.Lolzinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolzinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolzinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolzinho/Lolzinho/LolzinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolzinho.Lolzinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolzinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
